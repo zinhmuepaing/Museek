cf5eae81f289a05450685da1f6966bf7771dddcd
ZIN HMUE PAING 2403809C <2403809C@student.tp.edu.sg>
2026-01-17 19:21:31 +0800

    Add admin playlist creation and randomize homepage content
    - Enable admins to create playlists based on user ratings
    - Randomize songs and artists displayed on the homepage

--- Museek/Domain/AlbumSong.cs
+++ Museek/Domain/AlbumSong.cs
@@ -1,8 +1,8 @@
-﻿namespace Museek.Domain
-{
-    public class AlbumSong : BaseDomainModel
+﻿    namespace Museek.Domain
     {
-        public int AlbumId { get; set; }
-        public int SongId { get; set; }
+        public class AlbumSong : BaseDomainModel
+        {
+            public int AlbumId { get; set; }
+            public int SongId { get; set; }
+        }
     }
-}
